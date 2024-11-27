f849fdba5fbbf04d62dd69c44448b007c5903ad3
Nikodem Brożyniak <nbrozyniak@gmail.com>
2024-11-27 10:40:09 +0000

    m

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -1,17 +1,17 @@
 use std::cell::RefCell;
 
 thread_local! {
-    static MSG: RefCell<String> = RefCell::new(String::new());
+    static CHAT: RefCell<Vec<String>> = RefCell::new(Vec::new());
 }
 
 #[ic_cdk::update]
-fn save_message(message: String) {
-    MSG.with(|static_message| static_message.borrow_mut() = message);
+fn save_msg(msg: String) {
+    CHAT.with(|static_msg| static_msg.borrow_mut().push(msg));
 }
 
 #[ic_cdk::query]
-fn get_message() -> String {
-    MSG.with(|static_message|static_message.borrow().clone())
+fn get_chat() -> Vec<String> {
+    CHAT.with(|static_msg| static_msg.borrow().clone())
 }
 
 #[ic_cdk::query]
