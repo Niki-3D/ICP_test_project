26403a72fe730a600e339a638f002d0c84081cae
Nikodem Brożyniak <nbrozyniak@gmail.com>
2024-11-27 10:01:38 +0000

    projekt

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -1,3 +1,19 @@
+use std::cell::RefCell;
+
+thread_local! {
+    static MSG: RefCell<String> = RefCell::new(String::new());
+}
+
+#[ic_cdk::update]
+fn save_message(message: String) {
+    MSG.with(|static_message| static_message.borrow_mut() = message);
+}
+
+#[ic_cdk::query]
+fn get_message() -> String {
+    MSG.with(|static_message|static_message.borrow().clone())
+}
+
 #[ic_cdk::query]
 fn greet(name: String) -> String {
     format!("Hello, {}!", name)
